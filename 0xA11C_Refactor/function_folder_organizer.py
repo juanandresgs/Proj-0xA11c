c99cb12abc4d1813bc4a617710c69934fa79b961
nicole <nicole@intezer.com>
2024-11-08 11:36:21 -0600

    Added folders for functions

--- 0xA11C_Refactor/function_folder_organizer.py
+++ 0xA11C_Refactor/function_folder_organizer.py
@@ -6,7 +6,7 @@ from FeatureProof.TypeInfo import *
 
 # Initialize FeatureProof middleware and set up logging
 fp = Middleware()
-fp.set_logging_level(level=logging.DEBUG)
+fp.set_logging_level(level=logging.INFO)
 logger = fp.logger
 
 def get_all_library_functions_from_pdata() -> list[int]:
@@ -34,16 +34,19 @@ def get_all_library_functions_from_pdata() -> list[int]:
             continue
 
         func_ea = fp.get_imagebase() + func_rva
-        functions.append(func_ea)
+        
+        func_name = fp.get_function_name_from_address(safe_int_conversion(func_ea))
+        if not ("main" in func_name.lower() or "start" in func_name.lower()):
+            functions.append(func_ea)
+        else:
+            logging.info(f"Skipped function name: {func_name} at: {func_ea}")
         current_addr += 12
-
-    logger.debug(f"Library functions extracted from .pdata: {[hex(func) for func in functions]}")
+    
     return functions
 
 def get_rest_of_the_functions(
     library_functions: list[int],
-    thunk_functions: list[int],
-    imported_functions: list[tuple[str, int]]
+    thunk_functions: list[int]
 ) -> list[int]:
     """
     Filters out functions from the binary that are already categorized as library, 
@@ -65,29 +68,65 @@ def get_rest_of_the_functions(
     
     # Compile a set of excluded functions
     excluded_functions = set(thunk_functions)
-    excluded_functions.update(func[1] for func in imported_functions)
+    # excluded_functions = set()
+    # excluded_functions.update(func[1] for func in imported_functions)
     excluded_functions.update(library_functions_hex)
     
-    logger.debug(f"Thunk functions: {[hex(func) for func in thunk_functions]}")
-    logger.debug(f"Imported functions: {[hex(func[1]) for func in imported_functions]}")
-    logger.debug(f"Excluded library functions (hex): {library_functions_hex}")
+    # Convert excluded_functions to uppercase to match case in functions
+    excluded_lower = {item.lower() for item in excluded_functions}
+    
+    logging.info(f"Total number of functions is: {len(functions)}")
+    logging.info(f"The total number of filtered functions is: {len(excluded_lower)}")
     
     # Append only functions not in excluded_functions
     for func in functions:
-        if func not in excluded_functions:
+        if func.lower() not in excluded_lower:
             result.append(func)
     
-    logger.debug(f"Functions not in any category (uncategorized): {[hex(func) for func in result]}")
     return result
+
+def safe_int_conversion(value):
+    if isinstance(value, str):  # Convert from hexadecimal string
+        return int(value, 16)
+    elif isinstance(value, int):  # Already an integer
+        return value
+    else:
+        logging.error(f"Value must be a string or an integer. Got {value}")
+    
+def move_functions_to_folder(functions_addresses, folder_name):
+    
+    fp.create_folder(folder_name)
+    
+    fid = fp.check_folder_exists(folder_name)
+    
+    if fid:
+        for addr in functions_addresses:
+            func_name = fp.get_function_name_from_address(safe_int_conversion(addr))
+            if func_name:
+                fp.move_to_folder(func_name, folder_name)
+            else:
+                logging.debug(f"Couldn't find function name at address: {addr}")
+    else:
+        logging.error(f"Couldn't create folder named: {folder_name}")
+        return
+    
+    logging.info(f"Moved functions to a folder named: {folder_name}")
+        
 
 # Gather categorized function lists
 library_functions = get_all_library_functions_from_pdata()
-print(f"Total number of functions in .pdata: {len(library_functions)}")
+logging.info(f"Total number of functions in .pdata: {len(library_functions)}")
 
 thunk_functions = fp.get_all_thunk_functions()
-print(f"Total number of THUNK functions: {len(thunk_functions)}")
+logging.info(f"Total number of THUNK functions: {len(thunk_functions)}")
 
 imported_functions = fp.get_all_imports()
-print(f"Total number of imported functions: {len(imported_functions)}")
+logging.info(f"Total number of imported functions: {len(imported_functions)}")
 
 # Retrieve functions not in any excluded categories
+rest_of_the_functions = get_rest_of_the_functions(library_functions, thunk_functions)
+logging.info(f"Number of functions not in any of these categories: {len(rest_of_the_functions)}")
+
+move_functions_to_folder(library_functions, "Probably Library Functions")
+move_functions_to_folder(thunk_functions, "Thunk Functions")
+# move_functions_to_folder([func[1] for func in imported_functions], "Imported Functions")
